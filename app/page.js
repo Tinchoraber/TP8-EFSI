996bdfc4288a0249dc14cdcdae2fb48eddc575d6
Martin Raber <mraber2006@gmail.com>
2024-08-02 19:41:40 -0300

    Commit inicial

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import { useEffect, useState } from 'react';
+import styles from './page.module.css';
+import Image from 'next/image';
+export default function Home() {
+    const [paises, setPaises] = useState([]);
+    useEffect( () =>  {
+        async function getPaises(){
+            
+        }
+    })
+}
